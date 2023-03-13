e7b2164da050638f78aad7a32587a67fc4ba18f0
Your Name <you@example.com>
2023-03-13 21:26:23 +0700

    new update

---
+++ src/components/view/competitiondetails.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="banner-details user-profile"
+        style="background-image: linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url(assets/images/homeimg.jpg);">
+        <div class="container-fluid cnter-2">
+            <nav aria-label="Breadcrumb">
+                <ol class="breadcrumb  justify-content-center py-0 mb-4">
+                    <li class="breadcrumb-item"><a class=" link-success" href="thidau.html">Thi đấu</a></li>
+                    <li class="breadcrumb-item " aria-current="page">Chi tiết</li>
+                </ol>
+            </nav>
+            <h1 class="bannerh1">Chi tiết trận đấu</h1>
+            <br>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="article-event clearfix">
+                <div class="detail-event">
+                    <div class="col-sm-4">
+                        <h4 class="event-content-title">Thời gian</h4>
+                        <p class="event-content-info">
+                            <span class="event-sub-content-title">Ngày bắt đầu:
+                                <em class="date">12/04/2023</em></span>
+                            <span class="event-sub-content-title">Ngày kết thúc:
+                                <em class="date">18/04/2023</em></span>
+                        </p>
+                    </div>
+                    <div class="col-sm-4">
+                        <h4 class="event-content-title">Thông tin thi đấu</h4>
+                        <p class="event-content-info">
+                            <span class="event-sub-content-title">Điện thoại:</span>
+                            <a href="#" title="Liên hệ qua SĐT">081984551
+                            </a>
+                            <span class="event-sub-content-title">Email:</span>
+                            <a href="#" title="Liên hệ qua email">abc@gmail</a>
+                            <span class="event-sub-content-title">Địa chỉ:</span>
+                            <a href="#" target="_blank" title="">1 Nguyễn Đình Chiểu quận 3</a>
+                        </p>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <h4 class="event-content-title">Hình thức thi đấu</h4>
+                        <p class="event-content-info">
+                            <span class="event-sub-content-title">Loại chim:</span>
+                            chim sẻ <br>
+                            <span class="event-sub-content-title">Hình thức thi đấu:</span>
+                            Theo Bậc <br>
+                        </p>
+                    </div>
+
+                </div>
+                <div class="row align-items-center justify-content-center" style="margin-top: 50px">
+                    <div class="col-2">
+                        <a href="#">
+                            <button class="btn btn-success">Chỉnh sửa</button>
+                        </a>
+                    </div>
+                    <div class="col-2">
+                        <a href="#">
+                            <button class="btn btn-success">Tham gia</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
